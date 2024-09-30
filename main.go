c42d9b69567bd9308fc4296e8fd12c394cc2e8b5
Daniel Cesca <danielcescajunior18@gmail.com>
2024-09-30 14:05:59 -0300

    feat: restart game system

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 
 	"go-game/entities"
 )
@@ -34,6 +35,7 @@ type Game struct {
 }
 
 func (g *Game) Update() error {
+
 	if g.GameState == "RUNNING" {
 
 		g.HandleControls()
@@ -52,6 +54,10 @@ func (g *Game) Update() error {
 
 		g.Tick++
 		g.attackCounter++
+	} else if g.GameState == "GAMEOVER" {
+		if inpututil.IsKeyJustPressed(ebiten.KeyR) {
+			g.RestartGame()
+		}
 	}
 
 	return nil
@@ -95,6 +101,7 @@ func (g *Game) Draw(screen *ebiten.Image) {
 		GameoverSoundPlayer.Play()
 		ebitenutil.DebugPrintAt(screen, "GAME OVER", 130, 110)
 		ebitenutil.DebugPrintAt(screen, fmt.Sprintf("Score: %d", g.Points), 130, 125)
+		ebitenutil.DebugPrintAt(screen, fmt.Sprintf("Press R to try again..."), 90, 170)
 
 	}
 }
@@ -103,68 +110,7 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 	return 320, 240
 }
 
-func main() {
-	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Ebiten Zelda Clone")
-	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
-
-	// load the image from file
-	playerImg, _, err := ebitenutil.NewImageFromFile("assets/images/player.png")
-	if err != nil {
-		// handle error
-		log.Fatal(err)
-	}
-
-	attackImg, _, err := ebitenutil.NewImageFromFile("assets/images/attack.png")
-	if err != nil {
-		// handle error
-		log.Fatal(err)
-	}
-
-	tilemapImg, _, err := ebitenutil.NewImageFromFile("assets/images/TilesetFloor.png")
-	if err != nil {
-		// handle error
-		log.Fatal(err)
-	}
-
-	tilemapJSON, err := NewTilemapJSON("assets/maps/spawn.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	tilesets, _, err := tilemapJSON.GenTilesets()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if err := CreateMusicSound("assets/sounds/music.ogg"); err != nil {
-		log.Fatal(err)
-	}
-
-	if err := CreateHitSound("assets/sounds/hit.wav"); err != nil {
-		log.Fatal(err)
-	}
-
-	if err := CreateKillSound("assets/sounds/kill.wav"); err != nil {
-		log.Fatal(err)
-	}
-
-	if err := CreatePlayerHitSound("assets/sounds/playerhit.wav"); err != nil {
-		log.Fatal(err)
-	}
-
-	if err := CreateGameoverSound("assets/sounds/gameover.wav"); err != nil {
-		log.Fatal(err)
-	}
-
-	if err := CreateHealSound("assets/sounds/heal.wav"); err != nil {
-		log.Fatal(err)
-	}
-
-	if err := CreateLevelUpSoundPlayer("assets/sounds/levelup.wav"); err != nil {
-		log.Fatal(err)
-	}
-
+func initializeGame() *Game {
 	// collider for buildings
 	hardColliders := make([]image.Rectangle, 0)
 	for layerIndex, layer := range tilemapJSON.Layers {
@@ -252,10 +198,92 @@ func main() {
 
 	game.softColliders = append(game.softColliders, game.player.Collider)
 
+	return &game
+}
+
+func (g *Game) RestartGame() {
+	*g = *initializeGame()
+	MusicPlayer.Rewind()
+	MusicPlayer.Play()
+}
+
+var (
+	playerImg   *ebiten.Image
+	attackImg   *ebiten.Image
+	tilemapImg  *ebiten.Image
+	tilemapJSON *TilemapJSON
+	tilesets    []Tileset
+)
+
+func main() {
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Ebiten Zelda Clone")
+	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
+
+	var err error
+	// load the image from file
+	playerImg, _, err = ebitenutil.NewImageFromFile("assets/images/player.png")
+	if err != nil {
+		// handle error
+		log.Fatal(err)
+	}
+
+	attackImg, _, err = ebitenutil.NewImageFromFile("assets/images/attack.png")
+	if err != nil {
+		// handle error
+		log.Fatal(err)
+	}
+
+	tilemapImg, _, err = ebitenutil.NewImageFromFile("assets/images/TilesetFloor.png")
+	if err != nil {
+		// handle error
+		log.Fatal(err)
+	}
+
+	tilemapJSON, err = NewTilemapJSON("assets/maps/spawn.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tilesets, _, err = tilemapJSON.GenTilesets()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := CreateMusicSound("assets/sounds/music.ogg"); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := CreateHitSound("assets/sounds/hit.wav"); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := CreateKillSound("assets/sounds/kill.wav"); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := CreatePlayerHitSound("assets/sounds/playerhit.wav"); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := CreateGameoverSound("assets/sounds/gameover.wav"); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := CreateHealSound("assets/sounds/heal.wav"); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := CreateLevelUpSoundPlayer("assets/sounds/levelup.wav"); err != nil {
+		log.Fatal(err)
+	}
+
+	game := initializeGame()
+
 	MusicPlayer.Play()
 	SetVolumeValue(game.globalVolume)
 
-	if err := ebiten.RunGame(&game); err != nil {
+	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
 }
